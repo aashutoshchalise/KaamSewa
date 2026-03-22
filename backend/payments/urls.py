4a7341498c8aba681ffc4c0833979c64e7ce91da
aashutoshchalise <aashuchalise@gmail.com>
2026-03-22 15:23:00 +0545

    changed url settings

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ConfirmPaymentView
+from .views import PaymentDetailByBookingView, ConfirmPaymentView
 
 urlpatterns = [
+    path("booking/<int:booking_id>/", PaymentDetailByBookingView.as_view()),
     path("<int:payment_id>/confirm/", ConfirmPaymentView.as_view()),
 ]
